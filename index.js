bc5dc55339a96edaa56b5b20ecff4406b50f8e09
stella-mumbi <stella.mumbi@student.moringaschool.com>
2023-12-22 09:34:44 +0300

    cpmpleted assignment

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
 function addingEventListener() {
-}
+    const element = document.getElementById("button");
+    element.addEventListener("click", handleButtonClick);
+  }
+  
+  function handleButtonClick(event) {
+    alert("Button clicked");
+  }
+  
